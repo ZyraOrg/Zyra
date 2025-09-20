e84c0dd3fe774c1cfe475bac0bf40a856670f071
Sidney-130 <akabuoguoluebube8@gmail.com>
2025-09-20 21:25:25 +0100

    copy code from app.tsx to herosection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,12 @@
-import './index.css'
+import ExploreSection from "./components/ExploreSection/ExploreSection";
+import HeroSection from "./components/HeroSection/HeroSection";
+import "./index.css";
 
 export default function App() {
   return (
-    <div className="app-container">
-      {/* Navbar */}
-      <nav className="navbar">
-          <li><a href="#home" className="navbar-link">Home</a></li>
-          <li><a href="#features" className="navbar-link">Campaigns</a></li>
-          <li><a href="#pricing" className="navbar-link">Create</a></li>
-          <li><a href="#contact" className="navbar-link">About</a></li>
-      </nav>
-
-      {/* Hero Section */}
-      <section className="hero-section" id="home">
-        <h1 className="hero-h1">
-          RAISE VERIFIED<br>
-          </br> FUNDS WITH<br>
-          </br> <span className="hero-span">
-          TRANSPARENCY<br></br>
-          & TRUST</span>
-        </h1>
-        <p className="hero-p">
-          Every donation traceable on-chain, every beneficairy verified.
-          The future of crowdfunding is herer.
-        </p>
-      </section>
-
-      {/* button */}
-      <div className="button-1">Start a Campaign</div>
-      <div className="button-2">Donate Now</div>
-     
-    </div>
-  )
+    <main className="flex flex-col">
+      <HeroSection />
+      <ExploreSection />
+    </main>
+  );
 }
---
+++ frontend/src/components/ExploreSection/ExploreSection.jsx
@@ -0,0 +1,11 @@
+export default function ExploreSection() {
+  return (
+    <div className="bg-red-500">
+      <div>
+        <h1>
+          Explore <span>Active Campaigns</span>
+        </h1>
+      </div>
+    </div>
+  );
+}
