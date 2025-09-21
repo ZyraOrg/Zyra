499c4bed4926d70d344ea3a60f9fe619609e8cc2
Sidney-130 <akabuoguoluebube8@gmail.com>
2025-09-21 14:56:08 +0100

    remove alsolute positioning

--- frontend/src/components/ExploreSection/ExploreSection.jsx
+++ frontend/src/components/ExploreSection/ExploreSection.jsx
@@ -2,7 +2,7 @@ export default function ExploreSection() {
   return (
     <div className="bg-red-500">
       <div>
-        <h1>
+        <h1 className="text-red-500">
           Explore <span>Active Campaigns</span>
         </h1>
       </div>
