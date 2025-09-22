182dc97cb4c013978e5cdd1e59c60b2bf559a92f
Sidney-130 <akabuoguoluebube8@gmail.com>
2025-09-22 14:04:12 +0100

    fix card component

--- frontend/src/components/ExploreSection/ExploreSection.jsx
+++ frontend/src/components/ExploreSection/ExploreSection.jsx
@@ -1,11 +1,12 @@
+import { FaHourglassHalf } from "react-icons/fa";
 import { Card } from "./constants";
 
 export default function ExploreSection() {
   return (
-    <main className="w-full overflow-hidden my-10 flex flex-col justify-between items-center text-center">
+    <main className="w-full overflow-hidden my-10 px-5 flex flex-col justify-center items-center text-center">
       <div className="space-y-5 pb-5">
-        <h1 className="text-5xl font-semioold leading-15">
-          Explore <span className="">Active Campaigns</span>
+        <h1 className="text-2xl md:text-5xl font-semibold md:leading-15">
+          Explore <span className="text-secondary">Active Campaigns</span>
         </h1>
         <p className="text-md font-semibold">
           Support verified causes and track your impact in real-time on
@@ -13,11 +14,11 @@ export default function ExploreSection() {
           the blockchain
         </p>
       </div>
-      <div className="relative grid grid-cols-1 md:grid-cols-3 gap-3">
+      <div className="relative grid grid-cols-1 md:grid-cols-3 gap-12">
         {Card.map((card) => (
           <div
             key={card.id}
-            className="max-w-sm mx-auto overflow-hidden relative flex flex-col justify-between items-center bg-black rounded-4xl border-2 border-blue-500 shadow-md"
+            className="max-w-sm mx-auto overflow-hidden relative flex flex-col justify-between items-center bg-black rounded-4xl border-2 border-secondary shadow-md"
           >
             <div className="relative">
               <img
@@ -31,17 +32,19 @@ export default function ExploreSection() {
               <div className="text-left text-lg font-semibold">
                 {card.title}{" "}
               </div>
-              <div className="flex justify-between">
-                <div className="flex">
-                  <img src="/" alt="icon1" />
-                  <span>{card.name}</span>
+              <div className="flex justify-between items-start">
+                <div className="flex space-x-1 items-center">
+                  <span> {card.icon1} </span>
+                  <span className="text-md font-semibold text-gray-400">
+                    {card.name}
+                  </span>
                 </div>
-                <div className="flex flex-col">
-                  <div className="flex">
-                    <img src="/" alt="icon2" />
-                    <span> {card.time} </span>
+                <div className="flex flex-col text-right">
+                  <div className="flex items-center space-x-1">
+                    <span className="text-primary"> {card.Icon2} </span>
+                    <span className="text-md"> {card.time} </span>
                   </div>
-                  <div>{card.timeText}</div>
+                  <div className="text-gray-400">{card.timeText}</div>
                 </div>
               </div>
               <button className="mb-5 w-full bg-white text-blue-600 font-semibold rounded-full px-6 py-3">
