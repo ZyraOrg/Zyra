bb2f12c7dea5d4e6d70071bdfde7037d08c1cf52
Sidney-130 <akabuoguoluebube8@gmail.com>
2025-09-23 18:57:34 +0100

    style howItWorks section

--- frontend/src/components/HowItWorksSection/HowItWorksSection.jsx
+++ frontend/src/components/HowItWorksSection/HowItWorksSection.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const HowItWorksSection = () => {
-  return <div></div>;
+  return (
+    <main className="w-full overflow-hidden my-10 px-5 flex flex-col justify-center items-center text-center">
+      <div className="space-y-5 pb-5">
+        <h1 className="text-2xl md:text-5xl font-semibold md:leading-15">
+          How It <span className="text-secondary">Works</span>
+        </h1>
+        <p className="text-md font-semibold">
+          Four simple steps to transparent, blockchain-powered{" "}
+          <br className="hidden md:block" />
+          crowdfunding
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default HowItWorksSection;
