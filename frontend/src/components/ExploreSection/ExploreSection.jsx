11e77a36e2d64e2fd5bff17fa517120602309bcc
Kizito <ngerechisom55@gmail.com>
2025-09-24 01:38:21 +0100

    Fixed lucide-react error

--- frontend/src/components/ExploreSection/ExploreSection.jsx
+++ frontend/src/components/ExploreSection/ExploreSection.jsx
@@ -1,33 +1,34 @@
 import { FaHourglassHalf } from "react-icons/fa";
 import { Card } from "./constants";
-import { ArrowUpRight } from "lucide-react";
+
+ import { ArrowUpRight } from "lucide-react";
 
 export default function ExploreSection() {
   return (
-    <main className="w-full overflow-hidden my-10 px-5 flex flex-col justify-center items-center text-center">
-      <div className="space-y-5 pb-5">
-        <h1 className="text-2xl md:text-5xl font-semibold md:leading-15">
+    <main className="flex flex-col items-center justify-center w-full px-5 my-10 overflow-hidden text-center">
+      <div className="pb-5 space-y-5">
+        <h1 className="text-2xl font-semibold md:text-5xl md:leading-15">
           Explore <span className="text-secondary">Active Campaigns</span>
         </h1>
-        <p className="text-md font-semibold">
+        <p className="font-semibold text-md">
           Support verified causes and track your impact in real-time on
           <br className="hidden md:block" />
           the blockchain
         </p>
       </div>
-      <div className="relative grid grid-cols-1 md:grid-cols-3 gap-12">
+      <div className="relative grid grid-cols-1 gap-12 md:grid-cols-3">
         {Card.map((card) => (
           <div
             key={card.id}
-            className="max-w-sm mx-auto overflow-hidden relative flex flex-col justify-between items-center bg-black rounded-4xl border-2 border-secondary shadow-md"
+            className="relative flex flex-col items-center justify-between max-w-sm mx-auto overflow-hidden bg-black border-2 shadow-md rounded-4xl border-secondary"
           >
             <div className="relative">
               <img
                 src={card.image}
                 alt="cardimage"
-                className="w-full object-cover"
+                className="object-cover w-full"
               />
-              <span className="absolute top-3 right-5 bg-gray-200 text-primary/70 text-sm px-5 py-1 rounded-full font-semibold">
+              <span className="absolute px-5 py-1 text-sm font-semibold bg-gray-200 rounded-full top-3 right-5 text-primary/70">
                 {card.imageText}
               </span>
             </div>
@@ -36,10 +37,10 @@ export default function ExploreSection() {
               <div className="pt-4 text-left text-[22px] font-semibold">
                 {card.title}{" "}
               </div>
-              <div className="flex justify-between items-start">
-                <div className="flex space-x-1 items-center text-center">
+              <div className="flex items-start justify-between">
+                <div className="flex items-center space-x-1 text-center">
                   <span> {card.icon1} </span>
-                  <span className="text-md font-semibold text-gray-400">
+                  <span className="font-semibold text-gray-400 text-md">
                     {card.name}
                   </span>
                 </div>
@@ -53,15 +54,15 @@ export default function ExploreSection() {
                   </div>
                 </div>
               </div>
-              <button className="mb-5 w-full bg-white text-primary font-semibold rounded-full px-6 py-3 transition-transform duration-300 ease-in-out hover:scale-103">
+              <button className="w-full px-6 py-3 mb-5 font-semibold transition-transform duration-300 ease-in-out bg-white rounded-full text-primary hover:scale-103">
                 {card.buttonText}
               </button>
             </div>
           </div>
         ))}
       </div>
-      <div className="py-20 max-w-sm">
-        <button className="flex items-center px-12 py-1 text-sm font-semibold text-black bg-gradient-to-r from-secondary to-primary shadow-sm shadow-secondary rounded-2xl transition-transform duration-300 ease-in-out hover:scale-101 hover:transition-colors hover:bg-gradient-to-l hover:from-secondary hover:to-primary hover:duration-800 hover:ease-in-out">
+      <div className="max-w-sm py-20">
+        <button className="flex items-center px-12 py-1 text-sm font-semibold text-black transition-transform duration-300 ease-in-out shadow-sm bg-gradient-to-r from-secondary to-primary shadow-secondary rounded-2xl hover:scale-101 hover:transition-colors hover:bg-gradient-to-l hover:from-secondary hover:to-primary hover:duration-800 hover:ease-in-out">
           <p>View All Campaigns</p>
           <ArrowUpRight />
         </button>
