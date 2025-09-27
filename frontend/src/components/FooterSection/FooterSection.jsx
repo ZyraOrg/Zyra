f9126aaf69e60b34e0145eff096f38b8868a2737
Sidney-130 <akabuoguoluebube8@gmail.com>
2025-09-27 07:09:50 +0100

    update footer section

--- frontend/src/components/FooterSection/FooterSection.jsx
+++ frontend/src/components/FooterSection/FooterSection.jsx
@@ -3,28 +3,28 @@ import React from "react";
 
 const FooterSection = () => {
   return (
-    <main className="px-20">
-      <div className="flex justify-center items-center text-center gap-20">
-        <div className="text-left">
-          <img src={logo} alt="logo" className="w-32" />
-          <p>
-            Transparent blockchain <br />
-            crowdfunding for verified causes <br />
+    <main className="px-7 md:px-30 pb-5 md:space-y-9">
+      <div className="flex flex-col md:flex-row justify-between md:items-center md:max-w-6xl gap-5">
+        <div className="flex flex-col justify-between">
+          <img src={logo} alt="logo" className="w-32 h-32 object-cover" />
+          <p className="text-left text-[12px] md:text-[16px] text-gray-400 font-semibold">
+            Transparent blockchain <br className="hidden md:block" />
+            crowdfunding for verified causes <br className="hidden md:block" />
             worldwide
           </p>
         </div>
-        <div className="space-y-3 text-left">
+        <div className="space-y-2">
           <h1 className="text-lg font-semibold">Platform</h1>
-          <div className="text-md flex flex-col gap-2">
+          <div className="text-md flex flex-col gap-2 text-gray-400 font-semibold">
             <a href="/">Home</a>
             <a href="/">Campaigns</a>
             <a href="/">Create</a>
             <a href="/">Explore</a>
           </div>
         </div>
-        <div className="space-y-3 text-left">
+        <div className="space-y-2">
           <h1 className="text-lg font-semibold">Legal</h1>
-          <div className="text-md text-left flex flex-col gap-2">
+          <div className="text-md flex flex-col gap-2 text-gray-400 font-semibold">
             <a href="/">Terms</a>
             <a href="/">Privacy</a>
             <a href="/">Security</a>
@@ -32,7 +32,9 @@ const FooterSection = () => {
           </div>
         </div>
       </div>
-      <p className="text-left">2025 Zyra. All rights reserved. Built on Base</p>
+      <p className="text-center md:text-left text-[12px] md:text-[16px] text-gray-500 pt-5">
+        2025 Zyra. All rights reserved. Built on Base
+      </p>
     </main>
   );
 };
