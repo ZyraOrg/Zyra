f1dd060c21bd1793f94d3e7b48ee80fe439e31ce
Kizito <ngerechisom55@gmail.com>
2025-10-26 02:13:50 +0100

    started signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import HeroSection from "./components/HeroSection/HeroSection";
 import ExploreSection from "./components/ExploreSection/ExploreSection";
@@ -5,18 +6,29 @@ import FAQSection from "./components/FAQSection/FAQSection";
 import FooterSection from "./components/FooterSection/FooterSection";
 import HowItWorksSection from "./components/HowItWorksSection/HowItWorksSection";
 import WhyChooseZyraSection from "./components/WhyChooseZyraSection/WhyChooseZyraSection";
+import Signup from "./components/Signup";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <HeroSection />
-      <WhyChooseZyraSection />
-      <ExploreSection />
-      <HowItWorksSection />
-      <FAQSection />
-      <FooterSection />
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <HeroSection />
+              <WhyChooseZyraSection />
+              <ExploreSection />
+              <HowItWorksSection />
+              <FAQSection />
+              <FooterSection />
+            </>
+          }
+        />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,7 @@
+export default function ZyraLogin() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-[#010415]">
+      <h1 className="text-3xl font-bold text-white">Login Page (Coming Soon)</h1>
+    </div>
+  );
+}
