8799d76f9f00baea339fba70a9f91ee7e2857276
Sidney-130 <akabuoguoluebube8@gmail.com>
2026-05-29 13:40:26 +0100

    remove backend folder

--- backend/src/routes/transaction.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMyTransactions } = require('../controllers/transaction.controller');
-const { protect } = require('../middleware/auth.middleware');
-
-router.get('/', protect, getMyTransactions);
-
-module.exports = router;
